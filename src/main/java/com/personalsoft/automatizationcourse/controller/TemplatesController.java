c02768bba6de6fad3dca6df7b90f22dd594ba93a
dilopera <dilopera@personalsoft.com.co>
2020-12-02 20:22:32 -0500

    calculo02122020

--- src/main/java/com/personalsoft/automatizationcourse/controller/TemplatesController.java
+++ src/main/java/com/personalsoft/automatizationcourse/controller/TemplatesController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -14,11 +15,19 @@ public class TemplatesController {
 		model.addAttribute("mensaje", "Hola "+name+", buenas noches" );
 		return "index";
 		
+	}
+	@GetMapping("/calculo")
+	public String resultado(Model resultado,
+					@RequestParam(value="valor1", required =false) double number1,
+					@RequestParam(value="valor2", required =false) double number2){
+				resultado.addAttribute("suma", "El resultado de la suma es " + (number1+number2));
+				resultado.addAttribute("multiplicar", "El resultado de la multiplicacion es " + (number1*number2));
+				resultado.addAttribute("dividir", "El resultado de la division es " + (number1/number2));
+				return "result";
+}
+				
 		
 			
 	}
+		
 	
-	
-	
-
-}
