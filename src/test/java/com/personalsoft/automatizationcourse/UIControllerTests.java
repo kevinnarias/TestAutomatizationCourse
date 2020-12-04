84b396f3473cc26d606f1d8f33c5871e754f862c
klacevedo <klacevedo@personalsoft.com.co>
2020-12-03 20:45:13 -0500

    Prueba con Selenium

--- src/test/java/com/personalsoft/automatizationcourse/UIControllerTests.java
+++ src/test/java/com/personalsoft/automatizationcourse/UIControllerTests.java
@@ -1,5 +1,33 @@
 package com.personalsoft.automatizationcourse;
 
-public class UIControllerTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@ExtendWith({ ScreenshotOnFailureExtension.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class UIControllerTests {
+	@LocalServerPort
+    private int port;
+	@Container
+	private BrowserWebDriverContainer container = new BrowserWebDriverContainer().withCapabilities(new ChromeOptions());
+
+	@Test
+	void Prueba1 () {
+		this.container.getWebDriver().get("http://host.docker.internal:" + port + "/personalsoft/calculo?valor1=5&valor2=3");
+				WebElement messageElement = this.container.getWebDriver().findElementById("suma");
+				Assertions.assertEquals("El resultado de la suma es 8.0", messageElement.getText());
+		
+		
+	}
 }
+
